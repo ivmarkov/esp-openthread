b127f549ea84cb1a25535f092dc3952f53fd35b4
ivmarkov <ivan.markov@gmail.com>
2025-03-08 07:34:05 +0000

    Update the enet basic example

--- openthread/src/enet.rs
+++ openthread/src/enet.rs
@@ -1,16 +1,14 @@
 //! An implementation of an OpenThread Driver for `embassy-net` using the `embassy-net-driver-channel` crate.
 
-use core::{net::Ipv6Addr, pin::pin};
+use core::pin::pin;
 
 use embassy_futures::select::{select3, Either3};
-use embassy_net_driver_channel::{driver::HardwareAddress, RxRunner, StateRunner, TxRunner};
+use embassy_net_driver_channel::{driver::HardwareAddress, RxRunner, TxRunner};
 
-use rand_core::RngCore;
-
-use crate::{OpenThread, OperationalDataset, OtError, OtResources, Radio};
+use crate::{OpenThread, Radio};
 
 pub use embassy_net_driver_channel::{
-    driver::LinkState as EnetLinkState, Device as EnetDriver, State as EnetDriverState,
+    driver::LinkState as EnetLinkState, Device as EnetDriver, State as EnetDriverState, StateRunner as EnetStateRunner,
 };
 
 /// Create a new OpenThread driver for `embassy-net`, by internally instantiating the `openthread` API types
@@ -23,87 +21,30 @@ pub use embassy_net_driver_channel::{
 /// invisible to `embassy-net` and `smoltcp`.
 ///
 /// Arguments:
-/// - `rng`: A mutable reference to a random number generator.
+/// - `ot`: The OpenThread instance.
 /// - `state`: A mutable reference to the `embassy-net-driver-channel` state resources.
-/// - `resources`: A mutable reference to the `openthread` resources.
 ///
 /// Returns:
-/// - In case there were no errors related to initializing the OpenThread library, a tuple containing:
-///   - The OpenThread controller
 ///   - The `embassy-net-driver-channel` state runner (note: this is not really a "runner" per se, but more of a controller to switch on/off the Driver)
 ///   - A runner that runs both the `openthread` stack as well as the `embassy-net` driver stack
 ///   - The `embassy-net` Driver for OpenThread
 pub fn new<'d, const MTU: usize, const N_RX: usize, const N_TX: usize>(
-    rng: &'d mut dyn RngCore,
+    ot: OpenThread<'d>,
     state: &'d mut EnetDriverState<MTU, N_RX, N_TX>,
-    resources: &'d mut OtResources,
-) -> Result<(EnetController<'d>, EnetRunner<'d, MTU>, EnetDriver<'d, MTU>), OtError> {
-    let ot = OpenThread::new(rng, resources)?;
-
+) -> (EnetStateRunner<'d>, EnetRunner<'d, MTU>, EnetDriver<'d, MTU>) {
     let (runner, device) = embassy_net_driver_channel::new(state, HardwareAddress::Ip);
 
     let (state_runner, rx_runner, tx_runner) = runner.split();
 
-    Ok((
-        EnetController {
-            ot,
-            state: state_runner,
-        },
+    (
+        state_runner,
         EnetRunner {
             ot,
             rx_runner,
             tx_runner,
         },
         device,
-    ))
-}
-
-pub struct EnetController<'a> {
-    ot: OpenThread<'a>,
-    state: StateRunner<'a>,
-}
-
-impl EnetController<'_> {
-    pub fn set_link_state(&mut self, state: EnetLinkState) {
-        self.state.set_link_state(state);
-    }
-
-    /// Set a new active dataset in the OpenThread stack.
-    ///
-    /// Arguments:
-    /// - `dataset`: A reference to the new dataset to be set.
-    pub fn set_dataset(&self, dataset: &OperationalDataset<'_>) -> Result<(), OtError> {
-        self.ot.set_dataset(dataset)
-    }
-
-    /// Brings the OpenThread IPv6 interface up or down.
-    pub fn enable_ipv6(&self, enable: bool) -> Result<(), OtError> {
-        self.ot.enable_ipv6(enable)
-    }
-
-    /// This function starts/stops the Thread protocol operation.
-    ///
-    /// TODO: The interface must be up when calling this function.
-    pub fn enable_thread(&self, enable: bool) -> Result<(), OtError> {
-        self.ot.enable_thread(enable)
-    }
-
-    /// Gets the list of IPv6 addresses currently assigned to the Thread interface
-    ///
-    /// Arguments:
-    /// - `buf`: A mutable reference to a buffer where the IPv6 addresses will be stored.
-    ///
-    /// Returns:
-    /// - The total number of IPv6 addresses available. If this number is greater than
-    ///   the length of the buffer, only the first `buf.len()` addresses will be stored in the buffer.
-    pub fn ipv6_addrs(&self, buf: &mut [(Ipv6Addr, u8)]) -> Result<usize, OtError> {
-        self.ot.ipv6_addrs(buf)
-    }
-
-    /// Wait for the OpenThread stack to change its state.
-    pub async fn wait_changed(&self) {
-        self.ot.wait_changed().await
-    }
+    )
 }
 
 /// A runner that runs both the `openthread` stack runner as well as the `embassy-net-driver-channel` runner.
